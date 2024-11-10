e60b53c8e1be717a769168607d4bf54a16c08006
ALimjz5 <aedanlim5@gmail.com>
2024-11-10 21:40:50 +0800

    Update StudentManager module

--- Aedan_StudentManager.js
+++ Aedan_StudentManager.js
@@ -1,66 +1,61 @@
 module.exports = {
-    students: [],
+    students: [
+        {
+            id: 33456,
+            name: "Devin Booker",
+            course: "GHJ01"
+        }
+    ],
+    //register new student
     registerStudent(newID, newName, course) {
         this.students.push({ id: newID, name: newName, course: course });
         console.log("Student admitted");
     },
+    //remove student
     removeStudent(id) {
-        let index = this.students.findIndex(stud => stud.id == id);
-        if (index !== -1) {
+        let index = this.students.findIndex(stud => stud.id == id);         // Code will attempt find the student with the matching id and return the index in which the student is in the array
+        if (index !== -1) {                                                 //Index wil return a -1 value if it does not find student with id
             this.students = this.students.filter(stud => stud.id !== id);
             console.log("Student successfully removed");
         } else {
             console.log("Student does not exist");
         }
     },
-    editStudentName(id, newName) {
+    //edit a student's particulars or transfer their course
+    editStudent(id, newName, newCourse) {
         let index = this.students.findIndex(stud => stud.id == id);
         if (index !== -1) {
-            this.students[index].name = newName;
+            if ((newName != null || newName != "") && (newCourse == null || newCourse == "")) {                     //Checks if only the name is being changed
+                this.students[index].name = newName;
+            } else if ((newName == null || newName == "") && (newCourse != null || newCourse != "")) {              //Checks if the student is only transferring course
+                this.students[index].course = newCourse;
+            } else if ((newName != null || newName != "") && (newCourse != null || newCourse != "")) {              //Checks if both student name and course is being changed
+                this.students[index].name = newName;
+                this.students[index].course = newCourse;
+            }
         } else {
             console.log("Student not found");
         }
     },
-    transferStudent(id, newCourse) {
-        let index = this.students.findIndex(stud => stud.id == id);
-        if (index !== -1) {
-            this.students[index].course = newCourse;
-        } else {
-            console.log("Student not found");
-        }
-    },
-    getStudentName(id) {
-        let index = this.students.findIndex(stud => stud.id == id);
-        if (index !== -1) {
-            return this.students[index].name;
-        } else {
-            console.log("Student not found");
-        }
-    },
-    getStudentCourse(id) {
-        let index = this.students.findIndex(stud => stud.id == id);
-        if (index !== -1) {
-            return this.students[index].course;
-        } else {
-            console.log("Student not found");
-        }
-    },
+    //Shows all students under a course
     getStudentsByCourse(course) {
-        let courseSearch = this.students.filter(stud => stud.course === course);
+        let courseSearch = this.students.filter(stud => stud.course === course);                                    //Filters out the students who are in the chosen course
         return courseSearch;
     },
+    //For searching for a student by their name (multiple students will be shown if they have the same name)
     getStudentByName(name) {
-        let nameSearch = this.students.filter(stud => stud.name === name);
+        let nameSearch = this.students.filter(stud => stud.name === name);                                          //Filters out the students who's name has been searched
         return nameSearch;
     },
     getStudentById(id) {
         let index = this.students.findIndex(stud => stud.id == id);
-        if (index !== -1) {
+        if (index !== -1) { 
             return this.students.at({ id: id });
         } else {
             console.log("Student not found");
         }
     },
+    //Shows all students
     getStudentList() {
         return this.students;
     }
--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const student = require("./Aedan_StudentManager");
 console.log("Startup successful");
 
+student.registerStudent(12345, "John Deere", "CF001");
+console.log(student.getStudentList());
+student.editStudent(12345, "LeBron James");
+console.log(student.getStudentList());
+student.editStudent(12345, "", "DB023");
+console.log(student.getStudentList());
+student.editStudent(12345, "Kevin Durant", "UR455");
+console.log(student.getStudentList());
